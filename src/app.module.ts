21eb75d325ae65915156c232b8907d8f98b82ec0
FahadHeiji <fahadmohamedar@gmail.com>
2024-04-11 19:01:08 +0300

    Update: Project Files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { WordModule } from './words/words.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Dictoinary1:2nCpo1KJUVlESMgk@dictionary.w68ucgi.mongodb.net/Dictionary',
+    ),
+    WordModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
