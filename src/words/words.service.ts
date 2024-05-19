cc1fb2bd89568ea43c1ccf0bd44bdadf1cc39fde
FahadHeiji <fahadmohamedar@gmail.com>
2024-05-19 17:37:53 +0300

    Add: Get Words by Letter endpoint

--- src/words/words.service.ts
+++ src/words/words.service.ts
@@ -2,7 +2,34 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CombinedWord } from '../schemas/Word.schema';
-
+const alphabet: any[] = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'DH',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'KH',
+  'L',
+  'M',
+  'N',
+  'O',
+  'Q',
+  'R',
+  'S',
+  'SH',
+  'T',
+  'U',
+  'W',
+  'X',
+  'Y',
+];
 @Injectable()
 export class WordService {
   constructor(
@@ -19,11 +46,25 @@ export class WordService {
       throw NotFoundException;
     }
   }
-  getLetterWords(letter: string) {
+  async getLetterWords(letter: string) {
     try {
       const regex = new RegExp(`^${letter}`, 'i');
-      const result = this.wordModel.find({ term: regex }).sort({ term: 1 });
-      return result;
+      const result = await this.wordModel.find({ term: regex });
+      const res = result.map((word) => {
+        return {
+          wordID: word.wordID,
+          ref: word.ref,
+          term: word.term,
+          type: word.type,
+          conjunction: word.conjunction,
+          info: word.info,
+          definition: word.definition,
+          definitions: word.definitions,
+          synonyms: word.synonyms,
+          example: word.example,
+        };
+      });
+      return res;
     } catch {
       throw NotFoundException;
     }
